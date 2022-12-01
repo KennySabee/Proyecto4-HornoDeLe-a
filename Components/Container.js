c785a87a7a0dba6b940689ded7965439ec25e0b6
KennySabee <kemerizalde.dwfsc10@ucamp.utel.edu.mx>
2022-11-30 20:57:09 -0500

    feat: Avance 1

--- Components/Container.js
+++
@@ -1,5 +0,0 @@
-const Container = () => {
-    return <h1> Container</h1>;
-  };
-  export default Container;
-  
---
+++ src/Components/NoPage.js
@@ -0,0 +1,5 @@
+const NoPage = () => {
+    return <h1> ERROR 404 ESTA PAGINA NO EXISTE </h1>;
+  };
+  export default NoPage;
+  
